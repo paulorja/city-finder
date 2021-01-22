0838ff26366a44fa82b75189ff9ea49298b7a73c
paulo jansen <pauloo.jansen@gmail.com>
2021-01-22 01:18:50 -0300

    add frontend form to search cities

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -2,7 +2,17 @@ class CitiesController < ApplicationController
   def index; end
 
   def search
-    @cities = City.contains_name(params[:name]).where(state_id: params[:state_id])
-    render json: @cities.all
+    @cities = search_cities
+    render json: @cities, only: %i[name state_id]
+  end
+
+  private
+
+  def search_cities
+    if State.where(id: params[:state_id]).blank?
+      City.contains_name(params[:name])
+    else
+      City.contains_name(params[:name]).where(state_id: params[:state_id])
+    end
   end
 end
